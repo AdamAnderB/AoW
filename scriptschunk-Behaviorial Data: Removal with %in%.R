8aaf21a6c0d6b0298277156366f5ccdbdec3c1ba
Adam Bramlett <abramlet@andrew.cmu.edu>
2023-11-14 00:02:25 -0500

    remove useless files

--- scriptschunk-Behaviorial Data: Removal with %in%.R
+++
@@ -1,9 +0,0 @@
-## ----Behaviorial Data: Removal with %in%----
-#removal of remaining incorrect individual items
-behaviorial_data<-behaviorial_data%>%
-  filter(Participant.Private.ID %in% participant_agg$Participant.Private.ID &
-           center_image %in% item_agg$center_image) 
-           #image_incorrect == 0 & text_incorrect == 0)
-
-behaviorial_data <-behaviorial_data%>%
-  select(-c(text_incorrect,image_incorrect,response_button_text,pic_verification,target_obj,type))
--- scriptschunk-ET Data: Clean-Tidy-Filtering with inner_join().R
+++
@@ -1,8 +0,0 @@
-## ----ET Data: Clean-Tidy-Filtering with inner_join()----
-#combine eye tracking with response and OSF data frames-- with data clean-up and manipulation
-et_data<-eyetracking_data%>%
-  select(time_elapsed,participant_id,spreadsheet_row,type,x_pred_normalised,y_pred_normalised)%>%
-  filter(type =="prediction" )
-et_data<-et_data%>%rename("Participant.Private.ID"="participant_id",
-                          "Spreadsheet.Row"="spreadsheet_row")
-et_data$Participant.Private.ID<-as.factor(et_data$Participant.Private.ID)
