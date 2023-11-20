764b1f568e212af932c10de437a5df355f4399af
Adam Bramlett <abramlet@andrew.cmu.edu>
2023-11-20 10:43:51 -0500

    updating scripts

--- scripts/chunk-All Data: Preparing for Visualization.R
+++ scripts/chunk-All Data: Preparing for Visualization.R
@@ -1,7 +1,4 @@
 ## ----All Data: Preparing for Visualization----
-all_data_tidy <- all_data_cleaned%>%
-  filter(time_elapsed_rounded>-400 & time_elapsed_rounded<800)
-
 all_data_tidy_viz <- all_data_tidy%>% 
   group_by(time_elapsed_rounded,verb_type, talker)%>%
   summarise(target_looks = mean(target), 
