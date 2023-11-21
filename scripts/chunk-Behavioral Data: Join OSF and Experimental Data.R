2eb0d0f889d852278f40802c0a2a0af0764d6b40
Adam Bramlett <abramlet@andrew.cmu.edu>
2023-11-21 01:05:15 -0500

    you jknowfdk

--- scripts/chunk-Behavioral Data: Join OSF and Experimental Data.R
+++
@@ -1,3 +0,0 @@
-## ----Behavioral Data: Join OSF and Experimental Data----
-behavioral_data <-experimental_tidy%>%
-  left_join(OSF_filt, by=c( "talker","verb_type","subject_img_file"))
--- scripts/chunk-GLMER: Leveling the Data.R
+++
@@ -1,9 +0,0 @@
-## ----GLMER: Leveling the Data----
-mem_data$verb_type<-as.factor(mem_data$verb_type)
-mem_data$talker<-as.factor(mem_data$talker)
-contrasts(mem_data$verb_type)
-contrasts(mem_data$talker)
-contrasts(mem_data$verb_type)<-c(-.5,.5)
-contrasts(mem_data$talker)<-c(-.5,.5)
-colnames(contrasts(mem_data$talker))<- c('native')
-colnames(contrasts(mem_data$verb_type))<- c('nonrestricting')
