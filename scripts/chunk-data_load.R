1ccbec176ded01840fcec7d3a4325bdc74cd1654
Adam Bramlett <abramlet@andrew.cmu.edu>
2023-11-21 05:26:18 -0500

    things done

--- scripts/chunk-data_load.R
+++ scripts/chunk-data_load.R
@@ -6,11 +6,3 @@ input_path<-file.path(path,"data")
 output_path<-file.path(path,"cleaned_data")
 shiny_path<-file.path(path,"shiny_apps")
 viz_path<-file.path(path,"AoW/figures")
-
-#change screens to question in task
-task_data<-read.csv(file.path(input_path,"task_data.csv"),
-                    header=TRUE, row.names=1)
-eyetracking_data<-read.csv(file.path(input_path,"et_data.csv"),
-                           header=TRUE, row.names=1)
-OSF_data<-read.csv(file.path(input_path,"OSF_data.csv"),
-                   header=TRUE, row.names=1)
