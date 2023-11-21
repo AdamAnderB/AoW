9bca1a907c0a237980ea0c198efa6aeb593ab1e5
Adam Bramlett <abramlet@andrew.cmu.edu>
2023-11-21 00:05:38 -0500

    update script

--- scripts/chunk-All Data: Preparing for Models.R
+++ scripts/chunk-All Data: Preparing for Models.R
@@ -1,11 +1,9 @@
 ## ----All Data: Preparing for Models----
-participant_chinese_experience<-tidy_quest_data%>%
-  select(Participant.Private.ID,experience_chinese_accent)
-
 mem_data<-all_data_tidy%>%
   select(Participant.Private.ID,verb_type,talker,
          subject_img_file,target,Trial.Number,log_SUBTLWF_Obj,target_obj,time_elapsed)%>%
   mutate(Participant.Private.ID=as.factor(Participant.Private.ID))%>%
-  left_join(participant_chinese_experience)
+  left_join(tidy_quest_data)
+
 gamm_data<-mem_data%>%
   mutate(Condition = paste(talker,verb_type,sep="."))
