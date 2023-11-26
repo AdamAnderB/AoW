fe80a5aed5e246b6d1f7b50501debeb789ccad84
Adam Bramlett <abramlet@andrew.cmu.edu>
2023-11-26 12:37:15 -0500

    things donekisjhdfnkuhaskjdhffdafdassdfdsfbdsf

--- scripts/chunk-GAMM: Visualization.R
+++ scripts/chunk-GAMM: Visualization.R
@@ -1,7 +1,12 @@
 ## ----GAMM: Visualization----
-gamm_effects <- as.data.frame(summary(mod1)$p.table)
-#smooth_significance <- as.data.frame(summary(mod1)$s.table)
+gamm1_effects <- as.data.frame(summary(mod1)$p.table)
+gamm1_effects$Model<-"Main"
 
+#gamm2_effects <- as.data.frame(summary(mod2)$p.table)
+#gamm2_effects$Model<-"Accent"
+#gamm_effects<-rbind(gamm1_effects,gamm2_effects)
+#smooth_significance <- as.data.frame(summary(mod1)$s.table)
+gamm1_effects<-gamm1_effects
 gamm_effects$effect<-as.factor(rownames(gamm_effects))
 gamm_effects$effect <- fct_rev(fct_inorder(gamm_effects$effect))
 gamm_effects <- gamm_effects %>%
@@ -13,12 +18,15 @@ gamm_effects <- gamm_effects %>%
   mutate(filler = case_when(
     Estimate <= 0.0 ~ -1,
     Estimate >= 0.0 ~ 1))
-
-gamm_effects%>%ggplot(aes(x=Estimate,y=effect,color=as.factor(filler)))+
-  geom_pointrange(aes(xmin = Estimate-1.96 * `Std. Error`, xmax = Estimate+1.96 * `Std. Error`))+
+gamm_effects<-na.omit(gamm_effects)
+gamm_effects%>%ggplot(aes())+
+  geom_pointrange(aes(x=Estimate,y=effect,color=as.factor(filler),xmin = Estimate-1.96 * `Std. Error`, xmax = Estimate+1.96 * `Std. Error`))+
+  geom_point(aes(x=Estimate,y=effect,fill=Model),size=3)+
   geom_vline(xintercept = 0, linetype = "dashed", color = "black")+
-  geom_text(aes(x=-2,label = significance))+
+  geom_text(aes(x=-2,y=effect,label = significance))+
   theme_minimal()+
   theme(panel.grid.minor.y = element_blank(),
-        legend.position = "none")
-ggsave(file.path(viz_path,"model_1.pdf"), width = 6, height = 4)
+        panel.grid.minor.x = element_blank(),
+        legend.position = "none")+
+  scale_color_manual(values=c("#007BC0", "#FDB515"))
+ggsave(file.path(viz_path,"model_gamm_effects.pdf"), width = 6, height = 2)
--- scripts/chunk-GLMER: Models.R
+++ scripts/chunk-GLMER: Models.R
@@ -1,22 +1,23 @@
 ## ----GLMER: Models----
-m1_1<-glmer(target~talker*verb_type*experience_chinese+
+m1_1<-glmer(target~talker*verb_type+
             (talker|subject_img_file)+
             (verb_type|Participant.Private.ID)+
             (1|time_normalized),
           family="binomial",data=mem_data)
 
 #remove correlations
-m1_2<-glmer(target~talker*verb_type*experience_chinese+
+m1_2<-glmer(target~talker*verb_type+
             (talker||subject_img_file)+
             (verb_type||Participant.Private.ID)+
             (1|time_normalized),
           family="binomial",data=mem_data)
 #remove timing random effect
-m1_3<-glmer(target~talker*verb_type*experience_chinese+
+m1_3<-glmer(target~talker*verb_type+
             (talker||subject_img_file)+
             (verb_type||Participant.Private.ID),
           family="binomial",data=mem_data)
 
+anova(m1_2,m1_1)
 anova(m1_3,m1_2)
 #m2 is a more parsimonious and preferable model compared to m1_1 or m1_3 because of lower AIC and BIC
 #as well as the anova coming up significant in comparison to simpler model m1_3.
@@ -28,3 +29,4 @@ m2<-glmer(target~verb_type*experience_chinese+
             (verb_type|Participant.Private.ID)+
             (1|time_normalized),
           family="binomial",data=accent_mem_data)
+summary(m2)
